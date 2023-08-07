61ef8205e1596a555cfa3c15c0bdc99d92ac9540
Richard Smith <appsmithuk@outlook.com>
2023-08-07 17:57:09 +0100

    Big upgrade to prisma template now v5 is supported

--- prisma/flake.nix
+++ prisma/flake.nix
@@ -11,6 +11,7 @@
           pkgs.nodePackages.prisma
           pkgs.nodejs-slim
           pkgs.openssl
+          pkgs.sqlite
         ];
       in {
         devShells.default = pkgs.mkShell {
@@ -19,22 +20,39 @@
           # Ensure node operates in dev mode
           NODE_ENV = "development";
 
-          # Let prisma know where to find stuff
-          PRISMA_MIGRATION_ENGINE_BINARY =
-            "${pkgs.prisma-engines}/bin/migration-engine";
+          # Let prisma know where to find the introspection engine
+          PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+          PRISMA_INTROSPECTION_ENGINE_BINARY =
+            "${pkgs.prisma-engines}/bin/introspection-engine";
           PRISMA_QUERY_ENGINE_BINARY =
             "${pkgs.prisma-engines}/bin/query-engine";
           PRISMA_QUERY_ENGINE_LIBRARY =
             "${pkgs.prisma-engines}/lib/libquery_engine.node";
-          PRISMA_INTROSPECTION_ENGINE_BINARY =
-            "${pkgs.prisma-engines}/bin/introspection-engine";
-          PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+          PRISMA_SCHEMA_ENGINE_BINARY =
+            "${pkgs.prisma-engines}/bin/schema-engine";
 
           shellHook = ''
+            if [ ! -f ./package.json ]; then
+              # Initialise a node project
+              pnpm init
+              pnpm install @prisma/client@$(prisma --version 2>&1 | ${pkgs.gawk}/bin/awk --field-separator=: '/^prisma\s/ { gsub(/ /, "", $2); print $2 }')
+            fi
+
+            if [ ! -f ./prisma/schema.prisma ]; then
+              # Initialise prisma
+              read -e -p "Choose datasource provider -$(prisma init --help 2>&1 | ${pkgs.gawk}/bin/awk --field-separator=: '/^--datasource-provider/ { print $2 }') (sqlite): " dsprovider
+              prisma init --datasource-provider ''${dsprovider:-sqlite}
+            fi
+
+            if [ ! -d ./node_modules ]; then
+              # Restore missing node_modules directory
+              pnpm install
+            fi
+
             # Output some helpful info
             echo -e "\nnode $(node --version)"
             echo "pnpm v$(pnpm --version)"
-            echo -e "\n$(prisma --version)\n"
+            echo -e "\n$(prisma --version 2> /dev/null)\n"
           '';
         };
       });
