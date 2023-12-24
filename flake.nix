42a9f635a126b2ad47154c744136bbe5c745a654
Richard Smith <appsmithuk@outlook.com>
2023-12-24 22:57:57 +0000

    Minimize external dependencies

--- flake.nix
+++ flake.nix
@@ -2,87 +2,82 @@
   description = "Nix Flake Templates";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixos-templates.url = "github:nixos/templates";
     community-templates.url = "github:nix-community/templates";
   };
 
   outputs = {
     nixpkgs,
-    flake-utils,
     nixos-templates,
     community-templates,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      buildInputs = [pkgs.nixfmt pkgs.nodePackages.prettier];
-    in {
-      devShells.default = pkgs.mkShell {packages = buildInputs;};
-      formatter = pkgs.alejandra;
-    })
-    // (let
-      welcomeText = ''
-        Usage:
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux"];
+    welcomeText = ''
+      Usage:
 
-        $ nix develop
+      $ nix develop
 
-        or, if you have direnv installed:
+      or, if you have direnv installed:
 
-        $ direnv allow
+      $ direnv allow
 
-        See https://github.com/arti5an/flake-templates for more information.
-      '';
-      dotnetTemplate = {
-        inherit welcomeText;
-        path = ./dotnet;
-        description = "A standard .NET project";
+      See https://github.com/arti5an/flake-templates for more information.
+    '';
+    dotnetTemplate = {
+      inherit welcomeText;
+      path = ./dotnet;
+      description = "A standard .NET project";
+    };
+    hugoTemplate = {
+      inherit welcomeText;
+      path = ./hugo;
+      description = "A Hugo website project";
+    };
+    javascriptTemplate = {
+      inherit welcomeText;
+      path = ./javascript;
+      description = "A JavaScript project";
+    };
+    prismaTemplate = {
+      inherit welcomeText;
+      path = ./prisma;
+      description = "A JavaScript project with Prisma";
+    };
+    railsWithoutNodeTemplate = {
+      inherit welcomeText;
+      path = ./railswithoutnode;
+      description = "A ruby on rails project with no dependency on node";
+    };
+    railsTemplate = {
+      inherit welcomeText;
+      path = ./rails;
+      description = "A ruby on rails project";
+    };
+    rubyTemplate = {
+      inherit welcomeText;
+      path = ./ruby;
+      description = "A ruby project";
+    };
+  in {
+    inherit (nixos-templates) defaultTemplate;
+    formatter =
+      forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    templates =
+      nixos-templates.templates
+      // community-templates.templates
+      // {
+        csharp = dotnetTemplate;
+        dotnet = dotnetTemplate;
+        hugo = hugoTemplate;
+        javascript = javascriptTemplate;
+        js = javascriptTemplate;
+        node = javascriptTemplate;
+        prisma = prismaTemplate;
+        rails = railsTemplate;
+        railswithoutnode = railsWithoutNodeTemplate;
+        ruby = rubyTemplate;
       };
-      hugoTemplate = {
-        inherit welcomeText;
-        path = ./hugo;
-        description = "A Hugo website project";
-      };
-      javascriptTemplate = {
-        inherit welcomeText;
-        path = ./javascript;
-        description = "A JavaScript project";
-      };
-      prismaTemplate = {
-        inherit welcomeText;
-        path = ./prisma;
-        description = "A JavaScript project with Prisma";
-      };
-      railsWithoutNodeTemplate = {
-        inherit welcomeText;
-        path = ./railswithoutnode;
-        description = "A ruby on rails project with no dependency on node";
-      };
-      railsTemplate = {
-        inherit welcomeText;
-        path = ./rails;
-        description = "A ruby on rails project";
-      };
-      rubyTemplate = {
-        inherit welcomeText;
-        path = ./ruby;
-        description = "A ruby project";
-      };
-    in {
-      inherit (nixos-templates) defaultTemplate;
-      templates =
-        nixos-templates.templates
-        // community-templates.templates
-        // {
-          csharp = dotnetTemplate;
-          dotnet = dotnetTemplate;
-          hugo = hugoTemplate;
-          javascript = javascriptTemplate;
-          js = javascriptTemplate;
-          node = javascriptTemplate;
-          prisma = prismaTemplate;
-          rails = railsTemplate;
-          railswithoutnode = railsWithoutNodeTemplate;
-          ruby = rubyTemplate;
-        };
-    });
+  };
 }
